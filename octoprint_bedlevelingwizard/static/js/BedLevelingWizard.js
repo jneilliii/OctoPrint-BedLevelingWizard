bbeb223ff7dc6e16ea51796085153e70dbfd4d0f
Michael Denny <micdenny@gmail.com>
2018-07-24 17:38:44 +0200

    moved to tab template view instead of sidebar

--- octoprint_bedlevelingwizard/static/js/BedLevelingWizard.js
+++ octoprint_bedlevelingwizard/static/js/BedLevelingWizard.js
@@ -7,12 +7,12 @@
 $(function() {
     function BedlevelingwizardViewModel(parameters) {
         var self = this;
-		
+
         self.settingsViewModel = parameters[0];
 		self.printerProfilesViewModel = parameters[1];
 		self.controlViewModel = parameters[2];
 		self.loginStateViewModel = parameters[3];
-		
+
 		self.started = ko.observable();
 		self.stage = ko.observable();
 		self.current_point = ko.observable();
@@ -28,7 +28,7 @@ $(function() {
 		self.travel_speed = ko.computed(function(){return self.speed_xy()*60});
 		self.travel_speed_probe = ko.computed(function(){return self.speed_z_probe()*60});
 		self.gcode_cmds = ko.observableArray();
-		
+
 		self.onBeforeBinding = function() {
 			self.stage('Start');
 			self.started(false);
@@ -39,7 +39,7 @@ $(function() {
 			self.speed_xy(self.settingsViewModel.settings.plugins.bedlevelingwizard.speed_xy());
 			self.speed_z_probe(self.settingsViewModel.settings.plugins.bedlevelingwizard.speed_z_probe());
 		}
-		
+
 		self.onEventSettingsUpdated = function (payload) {
 			self.offset_xy(self.settingsViewModel.settings.plugins.bedlevelingwizard.offset_xy());
 			self.offset_z(self.settingsViewModel.settings.plugins.bedlevelingwizard.offset_z());
@@ -47,32 +47,7 @@ $(function() {
 			self.speed_xy(self.settingsViewModel.settings.plugins.bedlevelingwizard.speed_xy());
 			self.speed_z_probe(self.settingsViewModel.settings.plugins.bedlevelingwizard.speed_z_probe());
 		}
-		
-		self.onAfterBinding = function () {
-			// touchui hack
-			if($('html[id="touch"]').length > 0){
-				$('#touchui_dropdown_link').before('<li id="sidebar_plugin_bedlevelingwizard_wrapper_link" class=""><a href="#sidebar_plugin_bedlevelingwizard" data-toggle="tab"></a></li>');
-				$('#tabs_content').append($('#sidebar_plugin_bedlevelingwizard').addClass('tab-pane'));
-				
-				var $elm = $('#sidebar_plugin_bedlevelingwizard_wrapper_link')			
-				$elm
-					.clone()
-					.attr("id", $elm.attr("id")+"2")
-					.prependTo("#all_touchui_settings > .dropdown-menu")
-					.find("a")
-					.text('Leveling Wizard')
-					.off("click")
-					.on("click", function(e) {
-						$elm.find('a').click();
-						$("#all_touchui_settings").addClass("item_active");
-						e.preventDefault();
-						return false;
-					});
-				$elm.addClass('hidden_touch');
-				$(window).trigger('resize');
-			}
-		}
-		
+
 		self.start_level = function(){
 			if(!self.started()){
 				self.started(true);
@@ -84,7 +59,7 @@ $(function() {
 					var min_x = parseInt(volume.custom_box.x_min());
 					var max_x = parseInt(volume.custom_box.x_max());
 					var min_y = parseInt(volume.custom_box.y_min());
-					var max_y = parseInt(volume.custom_box.y_max());		
+					var max_y = parseInt(volume.custom_box.y_max());
 				} else {
 					console.log('Using width and depth');
 					var min_x = 0;
@@ -92,12 +67,12 @@ $(function() {
 					var min_y = 0;
 					var max_y = parseInt(volume.depth());
 				}
-				
+
 				self.point_a([min_x + parseInt(self.offset_xy()),min_y + parseInt(self.offset_xy())]);
 				self.point_b([max_x - parseInt(self.offset_xy()),max_y - parseInt(self.offset_xy())]);
 				self.point_c([max_x - parseInt(self.offset_xy()),min_y + parseInt(self.offset_xy())]);
 				self.point_d([min_x + parseInt(self.offset_xy()),max_y - parseInt(self.offset_xy())]);
-				
+
 				self.gcode_cmds.push('G90');
 				self.gcode_cmds.push('G1 Z'+self.offset_z_travel()+' F'+self.travel_speed_probe());
 				self.gcode_cmds.push('G28');
@@ -106,7 +81,7 @@ $(function() {
 				if(self.current_point() < 8){
 					self.gcode_cmds.push('G90');
 					self.gcode_cmds.push('G1 Z'+self.offset_z_travel()+' F'+self.travel_speed_probe());
-					
+
 					switch(self.current_point() % 4){
 						case 0:
 							self.gcode_cmds.push('G1 X'+self.point_a()[0]+' Y'+self.point_a()[1]+' F'+self.travel_speed());
@@ -127,25 +102,25 @@ $(function() {
 						default:
 							console.log('something went wrong');
 					}
-					
-					if(self.current_point() == 7) {						
+
+					if(self.current_point() == 7) {
 						self.stage('Finish');
 					}
-					
+
 					self.current_point(self.current_point()+1);
-				} 
+				}
 			} else {
 				self.gcode_cmds.push('G90');
 				self.gcode_cmds.push('G1 Z'+self.offset_z_travel()+' F'+self.travel_speed_probe());
 				self.gcode_cmds.push('G28 X0 Y0');
 				self.stop_level();
 			}
-			
+
 			console.log('sending commands for point '+self.current_point()+': '+self.gcode_cmds());
 			OctoPrint.control.sendGcode(self.gcode_cmds());
 			self.gcode_cmds([]);
 		}
-		
+
 		self.stop_level = function(){
 			console.log('Stopping Bed Leveling Wizard.');
 			self.started(false);
@@ -158,6 +133,6 @@ $(function() {
     OCTOPRINT_VIEWMODELS.push({
         construct: BedlevelingwizardViewModel,
         dependencies: ['settingsViewModel','printerProfilesViewModel','controlViewModel','loginStateViewModel'],
-        elements: ['#settings_plugin_bedlevelingwizard','#sidebar_plugin_bedlevelingwizard']
+        elements: ['#settings_plugin_bedlevelingwizard', '#tab_plugin_bedlevelingwizard']
     });
 });
