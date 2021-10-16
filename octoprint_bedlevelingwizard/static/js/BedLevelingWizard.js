6e0d8da48c9696fd2f0732b15cdcfd55561d0d99
jneilliii <jneilliii+github@gmail.com>
2021-10-15 22:56:52 -0400

    add octodash support via querystring url `?blwfullscreen` add release channels

--- octoprint_bedlevelingwizard/static/js/BedLevelingWizard.js
+++ octoprint_bedlevelingwizard/static/js/BedLevelingWizard.js
@@ -7,12 +7,12 @@
 $(function() {
     function BedlevelingwizardViewModel(parameters) {
         var self = this;
-		
+
         self.settingsViewModel = parameters[0];
 		self.printerProfilesViewModel = parameters[1];
 		self.controlViewModel = parameters[2];
 		self.loginStateViewModel = parameters[3];
-		
+
 		self.started = ko.observable();
 		self.stage = ko.observable();
 		self.current_point = ko.observable();
@@ -28,7 +28,7 @@ $(function() {
 		self.travel_speed = ko.computed(function(){return self.speed_xy()*60});
 		self.travel_speed_probe = ko.computed(function(){return self.speed_z_probe()*60});
 		self.gcode_cmds = ko.observableArray();
-		
+
 		self.onBeforeBinding = function() {
 			self.stage('Start');
 			self.started(false);
@@ -39,7 +39,7 @@ $(function() {
 			self.speed_xy(self.settingsViewModel.settings.plugins.bedlevelingwizard.speed_xy());
 			self.speed_z_probe(self.settingsViewModel.settings.plugins.bedlevelingwizard.speed_z_probe());
 		}
-		
+
 		self.onEventSettingsUpdated = function (payload) {
 			self.offset_xy(self.settingsViewModel.settings.plugins.bedlevelingwizard.offset_xy());
 			self.offset_z(self.settingsViewModel.settings.plugins.bedlevelingwizard.offset_z());
@@ -47,7 +47,7 @@ $(function() {
 			self.speed_xy(self.settingsViewModel.settings.plugins.bedlevelingwizard.speed_xy());
 			self.speed_z_probe(self.settingsViewModel.settings.plugins.bedlevelingwizard.speed_z_probe());
 		}
-		
+
 		self.start_level = function(){
 			if(!self.settingsViewModel.settings.plugins.bedlevelingwizard.use_custom_points()){ // use bed offsets
 				if(!self.started()){
@@ -60,7 +60,7 @@ $(function() {
 						var min_x = parseInt(volume.custom_box.x_min());
 						var max_x = parseInt(volume.custom_box.x_max());
 						var min_y = parseInt(volume.custom_box.y_min());
-						var max_y = parseInt(volume.custom_box.y_max());		
+						var max_y = parseInt(volume.custom_box.y_max());
 					} else {
 						console.log('Using width and depth');
 						var min_x = 0;
@@ -68,7 +68,7 @@ $(function() {
 						var min_y = 0;
 						var max_y = parseInt(volume.depth());
 					}
-					
+
 					self.point_a([min_x + parseInt(self.offset_xy()),min_y + parseInt(self.offset_xy())]);
 					self.point_b([max_x - parseInt(self.offset_xy()),max_y - parseInt(self.offset_xy())]);
 					self.point_c([max_x - parseInt(self.offset_xy()),min_y + parseInt(self.offset_xy())]);
@@ -88,7 +88,7 @@ $(function() {
 										stack: stack_bottomcenter
 										}
 									);
-					
+
 					self.gcode_cmds.push('G91');
 					self.gcode_cmds.push('G1 Z'+self.offset_z_travel()+' F'+self.travel_speed_probe());
 					self.gcode_cmds.push('G90');
@@ -98,7 +98,7 @@ $(function() {
 					if(self.current_point() < 8){
 						self.gcode_cmds.push('G90');
 						self.gcode_cmds.push('G1 Z'+self.offset_z_travel()+' F'+self.travel_speed_probe());
-						
+
 						switch(self.current_point() % 4){
 							case 0:
 								self.gcode_cmds.push('G1 X'+self.point_a()[0]+' Y'+self.point_a()[1]+' F'+self.travel_speed());
@@ -123,14 +123,14 @@ $(function() {
 							default:
 								console.log('something went wrong');
 						}
-						
+
 						if(self.current_point() == 7) {
 							self.stage('Finish');
 							var options = {text: 'You are at the fourth and final leveling position.  Adjust the bed to be a height of "0" and press Finish to home X and Y axis.'};
 						}
-						
+
 						self.current_point(self.current_point()+1);
-					} 
+					}
 				} else {
 					self.gcode_cmds.push('G90');
 					self.gcode_cmds.push('G1 Z'+self.offset_z_travel()+' F'+self.travel_speed_probe());
@@ -157,7 +157,7 @@ $(function() {
 										stack: stack_bottomcenter
 										}
 									);
-					
+
 					self.gcode_cmds.push('G90');
 					self.gcode_cmds.push('G1 Z'+self.offset_z_travel()+' F'+self.travel_speed_probe());
 					self.gcode_cmds.push('G28');
@@ -165,7 +165,7 @@ $(function() {
 				} else if(self.stage() !== 'Finish'){
 					self.gcode_cmds.push('G90');
 					self.gcode_cmds.push('G1 Z'+self.offset_z_travel()+' F'+self.travel_speed_probe());
-					
+
 					var custom_points = self.settingsViewModel.settings.plugins.bedlevelingwizard.custom_points();
 					if(self.current_point() < custom_points.length){
 						self.gcode_cmds.push('G1 X'+custom_points[self.current_point()].point_x()+' Y'+custom_points[self.current_point()].point_y()+' F'+self.travel_speed());
@@ -186,13 +186,13 @@ $(function() {
 					self.stop_level();
 				}
 			}
-			
+
 			console.log('sending commands for point '+self.current_point()+': '+self.gcode_cmds());
 			OctoPrint.control.sendGcode(self.gcode_cmds());
 			self.gcode_cmds([]);
 			self.notify.update(options);
 		}
-		
+
 		self.stop_level = function(){
 			console.log('Stopping Bed Leveling Wizard.');
 			self.started(false);
@@ -201,24 +201,35 @@ $(function() {
 			var options = {hide: true};
 			self.notify.update(options);
 		}
-		
+
 		self.addPoint = function(){
 			self.settingsViewModel.settings.plugins.bedlevelingwizard.custom_points.push({'point_x':ko.observable(0),'point_y':ko.observable(0)});
 		}
-		
+
 		self.removePoint = function(data){
-			self.settingsViewModel.settings.plugins.bedlevelingwizard.custom_points.remove(data);			
+			self.settingsViewModel.settings.plugins.bedlevelingwizard.custom_points.remove(data);
 		}
-		
+
 		self.move = function(amount, $index) {
 			var index = $index();
 			var item = self.settingsViewModel.settings.plugins.bedlevelingwizard.custom_points.splice(index, 1)[0];
 			var newIndex = Math.max(index + amount, 0);
 			self.settingsViewModel.settings.plugins.bedlevelingwizard.custom_points.splice(newIndex, 0, item);
 		};
-    
+
 		self.moveUp = self.move.bind(self, -1);
 		self.moveDown = self.move.bind(self, 1);
+
+    self.onAfterTabChange = function (current, previous) {
+        if (window.location.href.indexOf('blwfullscreen') > 0 && !$('#sidebar_plugin_bedlevelingwizard').hasClass('fullscreen')) {
+                $('#sidebar_plugin_bedlevelingwizard').addClass('fullscreen');
+                let background_color = ($('body').css('background-color') == 'rgba(0, 0, 0, 0)') ? '#FFFFFF' : $('#tabs_content').css('background-color');
+                $('#sidebar_plugin_bedlevelingwizard').css('background-color', background_color);
+                $('body').css('overflow-x', 'hidden');
+                $('body').css('overflow-y', 'hidden');
+                $('#sidebar_plugin_bedlevelingwizard button').addClass('btn-large');
+            }
+        };
     }
 
     OCTOPRINT_VIEWMODELS.push({
